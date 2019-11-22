087989c3ec7eacfb18d04c9122c51a695ef30f9a
LongChau <longchau1210@gmail.com>
2019-11-22 15:57:19 +0700

    ~ update

---
+++ Assets/_MyProject/Scripts/PathUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PathUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _txtPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _txtPath.SetText($"Path: {Application.persistentDataPath}");
+    }
+}
